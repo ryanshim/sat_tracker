e54c1ebb958fbe8de73b963d718f62660bd01eab
ryanshim <ryan.shim@gmail.com>
2016-12-24 02:15:57 -0800

    added azimuthElevation program

---
+++ azimuthElevation.py
@@ -0,0 +1,35 @@
+'''
+Get Azimuth and Elevation for user defined time interval
+
+Add user defined long/lat/alt (LLA)
+'''
+import ephem
+import datetime
+import time
+
+def getAzEl(timeInterval):
+    # read TLE data from file
+    with open('tle.txt', 'r') as inFile:
+        name = inFile.readline()
+        line1 = inFile.readline()
+        line2 = inFile.readline()
+
+    # set observer position
+    obs = ephem.Observer()
+    obs.lat = np.radians(33.66946)
+    obs.long = np.radians(-117.82311)
+    obs.elev = 26.78 # meters
+
+    # read the tle data
+    iss = ephem.readtle(name, line1, line2)
+
+    # calculate observer view direction
+    count = 0
+    while count <= timeInterval:
+        obs.date = datetime.datetime.utcnow()
+        iss.compute(obs)
+        print 'TIME: %s  AZ: %f  EL: %f\n' % \
+                (obs.date, np.degress(iss.az), np.degrees(iss.alt))
+        count += 1
+        time.sleep(1)
+
