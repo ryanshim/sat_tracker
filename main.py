c117e2a25e41a264c6efa8378a62bbba9af67e79
ryanshim <ryan.shim@gmail.com>
2017-04-15 17:20:16 -0700

    updated main function for better input sanitation

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ Utilizes pyephem and sgp4 packages to calculate object position
 
 TODO:
     + Use space-track api to collect TLE data                               OK
-    + Record historical orbit path
     + Create animated plot of current position (miller projection)
         - Closing graph does not close out of animate loop
     + Create a prediction function 
@@ -23,7 +22,7 @@ from plot_position import plot_body
 from animateTrack import plotMillerProj
 
 def main():
-    satList = []
+    satDict = {}    # empty dictionary to be populated w/ sat objects
 
     menu =  "1 = Scrape data from Space-Track\n" + \
             "2 = Get observer azimuth and elevation\n" + \
@@ -32,45 +31,40 @@ def main():
             "5 = Exit program\n"
             
     print menu
-    menuInput = input("Enter menu ID: ")
+    menuInput = input("Enter menu ID\n>>> ")
 
     # main loop
     while menuInput != 5:
         if menuInput == 1:
             getTleData()
             print "TLE data has been saved in 'tle.txt' file"
-            satList = parseTleData()
+            satDict = parseTleData()
 
         elif menuInput == 2:
-            #timeLength = 0
-            #timeLength = input("Enter length of time to track (seconds): ")
-            #getAzEl(timeLength)
-            '''
-            with open('tle.txt', 'r') as inFile:
-                l1 = inFile.readline()
-                l2 = inFile.readline()
-                satNum = l1[9:16]
-            print satNum
-            sat = Satellite(satNum, l1, l2)
-            sat.getAzEl()
-            '''
-            if not satList:
+            if not satDict:
                 print "Satellites not populated yet. Run menu option 1 first.\n"
             else:
-                satList[0].getAzEl()
+                satDictKey = raw_input("Enter satellite int'l designator" \
+                        "(FORMAT EX: 98067A)\n>>> ")
+                try:
+                    satDict[satDictKey].getAzEl()
+                except LookupError:
+                    print "Invalid key. Input correct key.\n" 
 
         elif menuInput == 3:
             timeLength = 0
             timeLength = input("Enter length of time to plot (seconds): ")
             plot_body(timeLength)
+
         elif menuInput == 4:
             print "Plotting current position..."
             plotMillerProj()
             continue
+
         else:
             print "Not a valid menu input"
-        menuInput = input("Enter menu ID: ")
 
+        menuInput = input("Enter menu ID\n>>> ")
     print "Exiting..."
 
 if __name__ == '__main__':
