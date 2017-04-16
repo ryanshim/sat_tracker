202486e56c7b65d9897a83d416c86b2dd348d355
ryanshim <ryan.shim@gmail.com>
2017-04-15 17:21:04 -0700

    updated spaceTrackScrape to handle exceptions (need refactoring)

--- spaceTrackScrape.py
+++ spaceTrackScrape.py
@@ -9,35 +9,40 @@ import subprocess
 import shlex
 import getpass
 
+''' Use space-track api to obtain tle data '''
 def getTleData():
-
     username = raw_input("Enter Space-Track e-mail: ")
     password = getpass.getpass("Enter password: ")
-
-    # ISS (ZARYA)
-    #bashCom = "curl https://www.space-track.org/ajaxauth/login -d 'identity="+ username +"&password=" + password + "&query='https://www.space-track.org/basicspacedata/query/class/tle_latest/NORAD_CAT_ID/25544/ORDINAL/1--5/orderby/ORDINAL/format/tle/emptyresult/show"
 
     # FULL CATALOG FROM SPACE-TRACK
     bashCom = "curl https://www.space-track.org/ajaxauth/login -d 'identity="+ username +"&password=" + password + "&query='https://www.space-track.org/basicspacedata/query/class/tle_latest/ORDINAL/1/EPOCH/%3Enow-30/orderby/NORAD_CAT_ID/format/tle"
 
+    try:
+        subprocess.call(shlex.split(bashCom))   # call the curl process
+        output = subprocess.check_output(['bash', '-c', bashCom])
+    except subprocess.CalledProcessError():
+        print "Could not download TLE data\n"
+        return
 
-    subprocess.call(shlex.split(bashCom))
-
-    output = subprocess.check_output(['bash', '-c', bashCom])
-
-    tleFile = open('tle.txt', 'w')
+    tleFile = open('tle.txt', 'w')  # save the data to file
     tleFile.write(output)
     tleFile.close()
 
+'''
+load saved data to dictionary
+Dictionary format: satDict[int'lDesignator] = Satellite object
+'''
 def parseTleData():
-    satList = []
+    satDict = {} 
     with open('tle.txt', 'r') as inFile:
         while True:
             l1 = inFile.readline()
             l2 = inFile.readline()
-            satNum = l1[9:16]
-            satList.append(Satellite(satNum, l1, l2))
-            if not l2:
+            if not l2:  # reached eof
                 break
-    return satList
+            # use int'l designator as key
+            itlDesig = l1.split(" ")[2]
+            satDict[itlDesig] = Satellite(itlDesig, l1, l2)
+
+    return satDict
 
