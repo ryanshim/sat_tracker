d3a268f495c989bfdbd16e1ca82f8e660d129f56
ryanshim <ryan.shim@gmail.com>
2017-04-20 13:54:13 -0700

    formatting...might need to run pylint later

--- spaceTrackScrape.py
+++ spaceTrackScrape.py
@@ -15,7 +15,10 @@ def getTleData():
     password = getpass.getpass("Enter password: ")
 
     # FULL CATALOG FROM SPACE-TRACK
-    bashCom = "curl https://www.space-track.org/ajaxauth/login -d 'identity="+ username +"&password=" + password + "&query='https://www.space-track.org/basicspacedata/query/class/tle_latest/ORDINAL/1/EPOCH/%3Enow-30/orderby/NORAD_CAT_ID/format/tle"
+    bashCom = "curl https://www.space-track.org/ajaxauth/login -d 'identity=" + \
+              username + "&password=" + password + "&query='https://www.space-track.org" \
+              "/basicspacedata/query/class/tle_latest/ORDINAL/1/EPOCH/%3Enow-30/" \
+              "orderby/NORAD_CAT_ID/format/tle"
 
     try:
         subprocess.call(shlex.split(bashCom))   # call the curl process
