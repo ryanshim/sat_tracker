2190809179a307fc1f7c9a36ee7e581af22dddb0
Ryan <ryan.shim@gmial.com>
2017-04-20 12:33:07 -0700

    begin updating plot position

--- plot_position.py
+++ plot_position.py
@@ -3,23 +3,22 @@ Plots object's current position over surface
 on orthographic projection of Earth
 '''
 import time
-from state_vectors import get_SV
+#from state_vectors import get_SV
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_body(timeInterval):
-
+def plotBody(satObject):
     # create mpl figure
     fig = plt.figure(figsize=plt.figaspect(1))
     ax = fig.add_subplot(111, projection='3d', aspect=1)
 
     # plot earth surface
-    max_radius = 0
-    earth_radius = 6371 # km
-    max_radius = max(max_radius, earth_radius)
+    maxRadius = 0
+    earthRadius = 6371 # km
+    maxRadius = max(maxRadius, earthRadius)
     coefs = (1, 1, 1)
-    rx, ry, rz = [earth_radius/np.sqrt(coef) for coef in coefs]
+    rx, ry, rz = [earthRadius/np.sqrt(coef) for coef in coefs]
     u = np.linspace(0, 2*np.pi, 100)
     v = np.linspace(0, np.pi, 100)
 
@@ -32,11 +31,10 @@ def plot_body(timeInterval):
     # draw Earth
     ax.plot_surface(x, y, z, rstride=4, cstride=4, color='g', antialiased=True)
 
-    # plot positions in recorded file
-    with open('tle.txt', 'r') as inFile:
-        #name = inFile.readline()
-        line1 = inFile.readline()
-        line2 = inFile.readline()
+    # get satellite data
+    designator = satObject.ItlDesig()
+    line1 = satObject.getLine1()
+    line2 = satObject.getLine2()
 
     print "Position in state vector format..."
     posData = []
