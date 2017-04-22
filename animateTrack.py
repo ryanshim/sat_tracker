016db40bd02415a30569fc6f02346ebae02e529a
ryanshim <ryan.shim@gmail.com>
2017-04-21 21:16:41 -0700

    spelling errors, began working on statevector, velocity, lla coord legend

--- animateTrack.py
+++ animateTrack.py
@@ -2,43 +2,52 @@
 Plot object's current position over an animated surface
 '''
 import ephem
+import datetime # include for nightshade
 from mpl_toolkits.basemap import Basemap
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 def plotMillerProj(satObject):
-
+    '''
+    SET UP THE MAP
+    '''
     # miller projection
+    plt.figure(figsize=(9,7))
     millsMap = Basemap(projection='mill', lon_0=180)
 
-    # plot coastlines, draw label meridians and parallels
+    # plot coastlines, draw label meridians, parallels, nightshade
     millsMap.drawcoastlines(color='w')
     millsMap.drawparallels(np.arange(-90,90,30), labels=[1,0,0,0])
     millsMap.drawmeridians(np.arange(-180,180,30), labels=[0,0,0,1])
+    millsMap.nightshade(datetime.datetime.utcnow(), alpha=0.2) # alpha = transparency lvl
 
     # fill continents 'black' (with zorder=0), color wet areas 'black'
     # color 'k' = black
     millsMap.drawmapboundary(fill_color='#1a335f')
     millsMap.fillcontinents(color='k', lake_color='#1a335f')
 
+    '''
+    PLOT OBJECT PATH
+    '''
     # satellite object getters
     designator = satObject.getItlDesig()
     line1 = satObject.getLine1()
     line2 = satObject.getLine2()
 
-    # map out propogated coordinates 
-    lonsLats = satObject.propogatePath()
+    # map out propagated coordinates 
+    lonsLats = satObject.propagatePath()
     for data in lonsLats:
         if data[0] < 0:
             data[0] += 360
 
         x, y = millsMap(data[0], data[1])
-        # yellow dot
         millsMap.plot(x, y, 'yo', markersize=2)[0]
 
+    '''
+    PLOT OBJECT CURRENT POSITION + UPDATE LEGEND
+    '''
     # reset x, y to plot current position
-    # red dot
     x, y = millsMap(0,0)
     position = millsMap.plot(x, y, 'ro', markersize=5)[0]
 
@@ -47,6 +56,7 @@ def plotMillerProj(satObject):
         return position,
 
     def animate(i):
+        # get object longitude latitude
         sat = ephem.readtle(designator, line1, line2)
         sat.compute()
         lons = np.degrees(sat.sublong)
@@ -62,13 +72,31 @@ def plotMillerProj(satObject):
         return position,
 
     try:
+        '''
+        # set object position/velocity status
+        satPosition = satObject.getSV()
+        rLabel = ['x', 'y', 'z']
+        cLabel = ['SV: ']
+        cells = [[satPosition[0]], [satPosition[1]], [satPosition[2]]]
+
+        plt.table(cellText=cells,
+                  colWidths=[0.2],
+                  rowLabels=rLabel,
+                  colLabels=cLabel,
+                  loc='bottom')
+        '''
+
         # call the animator. blit=True  means to redraw only the 
         # parts that have been changed
         anim = animation.FuncAnimation(plt.gcf(), animate,
                                        init_func=init, frames=20,
                                        interval=500, blit=True)
+        
 
+        # set plot title
         plt.title(designator)
         plt.show()
     except KeyboardInterrupt:
         return
+
+
