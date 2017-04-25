a93acd2f39d3840df0f1c3d296ac4dfc0d1b215d
ryanshim <ryan.shim@gmail.com>
2017-04-25 15:38:18 -0700

    animatedTrack now shows object info; need to format float precision

--- animateTrack.py
+++ animateTrack.py
@@ -25,7 +25,7 @@ def plotMillerProj(satObject):
     # fill continents 'black' (with zorder=0), color wet areas 'black'
     # color 'k' = black
     millsMap.drawmapboundary(fill_color='#1a335f')
-    millsMap.fillcontinents(color='k', lake_color='#1a335f')
+    millsMap.fillcontinents(color='#111111', lake_color='#1a335f')
 
     '''
     PLOT OBJECT PATH
@@ -35,24 +35,24 @@ def plotMillerProj(satObject):
     line1 = satObject.getLine1()
     line2 = satObject.getLine2()
 
-    # map out propagated coordinates 
+    # plot propagated coordinates to show object path 
     lonsLats = satObject.propagatePath()
     for data in lonsLats:
         if data[0] < 0:
             data[0] += 360
 
         x, y = millsMap(data[0], data[1])
-        millsMap.plot(x, y, 'yo', markersize=2)[0]
+        millsMap.plot(x, y, 'y_', markersize=2)[0]
 
     '''
-    PLOT OBJECT CURRENT POSITION + UPDATE LEGEND
+    PLOT OBJECT CURRENT POSITION + CREATE STATUS BOX 
     '''
     # reset x, y to plot current position
     x, y = millsMap(0,0)
     position = millsMap.plot(x, y, 'ro', markersize=5)[0]
 
     # create sat info text object
-    satInfo = plt.text(0, 1, '', color='w')
+    satInfo = plt.text(0, 0, '', color='w')
 
     def init():
         satInfo.set_text('')
@@ -68,21 +68,25 @@ def plotMillerProj(satObject):
 
         satStateVectors = satObject.getSV() # get object state vectors
         velocity = np.sqrt(satStateVectors[1][0]**2 +  # calc velocity magnitude 
-                        satStateVectors[1][1]**2 +
-                        satStateVectors[1][2]**2)
+                           satStateVectors[1][1]**2 +
+                           satStateVectors[1][2]**2)
         
-
-        satInfo.set_text('Lat: ' + str(lats) +
-                         '\nLon: ' + str(lons) +
-                         '\nVelocity: ' + str(velocity) + ' (km/s)')
+        # update text
+        satInfo.set_fontsize(8)
+        satInfo.set_text(' Lat: ' + str(lats) +
+                         '\n Lon: ' + str(lons) +
+                         '\n Velocity: ' + str(velocity) + ' (km/s)' +
+                         '\n x: ' + str(satStateVectors[0][0]) +
+                         '  y: ' + str(satStateVectors[0][1]) +
+                         '  z: ' + str(satStateVectors[0][2]) + '\n')
 
         # basemap uses 0,0 as bottom left corner of plot
         if lons < 0:
             lons += 360
 
         x, y = millsMap(lons, lats)
-
         position.set_data(x, y)
+
         return position, satInfo
 
     try:
@@ -91,12 +95,10 @@ def plotMillerProj(satObject):
         anim = animation.FuncAnimation(plt.gcf(), animate,
                                        init_func=init, frames=20,
                                        interval=500, blit=True)
-        
 
         # set plot title
         plt.title("Itn'l Designator: " + designator)
         plt.show()
     except KeyboardInterrupt:
         return
-
 
