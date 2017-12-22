30ac36e76d42ea4ec3e38c0782e20e717faeb203
ryanshim <ryan.shim@gmail.com>
2017-12-21 17:10:26 -0800

    modularized tracking

--- revision/static/js/set_map.js
+++ revision/static/js/set_map.js
@@ -18,6 +18,35 @@ function get_tle(tle_data) {
     return [tle_line1, tle_line2];
 }
 
+function get_sat_path(l1, l2) {
+    var sat_lat = new Array(8);
+    var sat_lon = new Array(8);
+    var time = new Date();
+
+    // each time step is 1 min; propagate for 1 hr
+    for (i = 0; i < 8; i++) {
+        time = new Date(time.setMinutes(time.getMinutes() + i));
+        var satrec = satellite.twoline2satrec(l1, l2);
+        var position_velocity = satellite.propagate(satrec, time);
+        var positionEci = position_velocity.position;
+        var gmst = satellite.gstimeFromDate(time);
+        var positionGd = satellite.eciToGeodetic(positionEci, gmst);
+        var latitude = rad2deg(positionGd.latitude);
+        var longitude = rad2deg(positionGd.longitude);
+
+        if (longitude < 180) {  // for map degree conversion
+            longitude += 360;
+        }
+        if (longitude > 180) {
+            longitude -= 360;
+        }
+
+        sat_lat.push(latitude);
+        sat_lon.push(longitude);
+    }
+    return [sat_lat, sat_lon];
+}
+
 // Conversion functions
 function deg2rad(degrees) {
     return degrees * (Math.PI / 180);
@@ -31,25 +60,19 @@ function rad2deg(rads) {
 function make_map(tle) {
     var raw_tle = this.get_tle(tle);
     var itl_desig = raw_tle[0].substring(9, 16);
+    var lats_lons = this.get_sat_path(raw_tle[0], raw_tle[1]);
 
-    var satrec = satellite.twoline2satrec(raw_tle[0], raw_tle[1]);  // init sat record
-    var positionAndVelocity = satellite.propagate(satrec, new Date());
-    
-    console.log(positionAndVelocity);   // state vectors; ECEF
+    // process array
+    lats_lons[0] = lats_lons[0].slice(8, 16);
+    lats_lons[1] = lats_lons[1].slice(8, 16);
+    latitude = lats_lons[0][0];
+    longitude = lats_lons[1][0];
 
-    var positionEci = positionAndVelocity.position;
-    var gmst = satellite.gstimeFromDate(new Date());    // readme uses fn:gstime()
-
-    var positionGd = satellite.eciToGeodetic(positionEci, gmst);
-
-    // positionGd.lat/lon returns in rads
-    var latitude = rad2deg(positionGd.latitude);
-    var longitude = rad2deg(positionGd.longitude);
-
+    console.log(lats_lons);
     console.log(latitude);
     console.log(longitude);
 
-    // Map settings
+    // Map settings; default projection = mercator
     AmCharts.makeChart( "mapdiv", {
       "type": "map",
 
@@ -61,12 +84,20 @@ function make_map(tle) {
             "latitude": latitude,
             "longitude": longitude,
             "type": "circle",
-            "color": "#000000"
+            "color": "#CC0000"
+        } ],
+
+        "lines": [ {
+            "latitudes": lats_lons[0],
+            "longitudes": lats_lons[1],
+            "color": "#FFCC00"
         } ]
       },
 
       "areasSettings": {
         "autoZoom": true,
+        "color": "#000000",
+        "outlineColor": "#FFFFFF",
         "selectedColor": "#CC0000"
       },
 
