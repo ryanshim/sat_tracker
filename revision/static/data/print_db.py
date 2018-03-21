1d8bc1c1aeefad68d0bf3b6afa879a508ef54bad
ryanshim <ryan.shim@gmail.com>
2018-03-20 20:07:23 -0700

    use sqlite database rather than txt file

---
+++ revision/static/data/print_db.py
@@ -0,0 +1,27 @@
+# Print the TLE database for testing
+import sqlite3
+import ephem
+import numpy as np
+
+conn = sqlite3.connect('tle.db')
+c = conn.cursor()
+count = 0
+
+for row in c.execute('SELECT * FROM tles'):
+    print(row)
+    count += 1
+
+print(count)
+
+conn.close()
+
+itl_desig = row[0]
+line1 = row[1]
+line2 = row[2]
+
+sat = ephem.readtle(itl_desig, line1, line2)
+sat.compute()
+lat = np.degrees(sat.sublat)
+lon = np.degrees(sat.sublong)
+height = sat.elevation
+print(lat, lon, height)
