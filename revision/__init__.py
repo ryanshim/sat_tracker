f639e31e927075b1b24796f8bc650e2c77776954
ryanshim <ryan.shim@gmail.com>
2018-04-03 02:52:19 -0700

    testing plotting orbit path for ISS

--- revision/__init__.py
+++ revision/__init__.py
@@ -1,8 +1,6 @@
 # Satellite Tracker Revision
 #
 # Revision Notes:
-#   - Convert to python3
-#   - Create web-based UI
 #
 import sqlite3
 import collections
@@ -10,7 +8,55 @@ import json
 from flask import Flask, render_template, request, url_for
 from .sat import Sat
 
+# for ISS tracking
+import ephem
+import datetime
+import numpy as np
+
 app = Flask(__name__)
+
+# Cesium Globe page
+@app.route('/', methods=['GET', 'POST'])
+def ces_track():
+    iss_positions = []
+    positions = []
+    error_count = 0
+
+    conn = sqlite3.connect('./static/data/tle.db')
+    c = conn.cursor()
+
+    for row in c.execute('SELECT * FROM tles'):
+        sat = Sat(row[0], row[1], row[2])
+        try:
+            lat, lon, height = sat.get_position()
+        except:
+            error_count += 1
+            pass
+        positions.append([lat, lon, height])
+
+    # Retrieve ISS tle
+    iss_tle = []
+    for row in c.execute("SELECT * FROM tles WHERE itl_desig = '98067A  '"):
+        iss_tle = row
+    
+    conn.close()
+    print(error_count)
+
+    # Get points to plot ISS path
+    base = datetime.datetime.today()
+    t_delta = [base - datetime.timedelta(minutes=x) for x in range(-30, 90)]
+
+    # Compute iss orbit positions
+    iss = ephem.readtle(iss_tle[0], iss_tle[1], iss_tle[2])
+    for time in t_delta:
+        iss.compute(time)
+        iss_positions.append(np.degrees(iss.sublong))
+        iss_positions.append(np.degrees(iss.sublat))
+        iss_positions.append(iss.elevation)
+
+    return render_template('cesium_test.html', 
+            pos_arr=json.dumps(positions),
+            iss_pos=json.dumps(iss_positions))
 
 '''
 # Main landing page
@@ -43,26 +89,3 @@ def tracking():
             tle=json.dumps(tle_raw))
 '''
 
-# Cesium Globe page
-@app.route('/', methods=['GET', 'POST'])
-def ces_track():
-    positions = []
-    error_count = 0
-
-    conn = sqlite3.connect('./static/data/tle.db')
-    c = conn.cursor()
-
-    for row in c.execute('SELECT * FROM tles'):
-        sat = Sat(row[0], row[1], row[2])
-        try:
-            lat, lon, height = sat.get_position()
-        except:
-            error_count += 1
-            pass
-        positions.append([lat, lon, height])
-    
-    conn.close()
-    print(error_count)
-
-    return render_template('cesium_test.html', 
-            pos_arr=json.dumps(positions))
--- revision/static/js/ces_test.js
+++ revision/static/js/ces_test.js
@@ -1,5 +1,6 @@
 // Plot position of sat on cesium map
-function plot_all(positions) {
+//  - Test plotting ISS orbit path
+function plot_all(positions, iss_positions) {
     // create new cesium globe
     var viewer = new Cesium.Viewer('cesiumContainer', {
         animation: false,
@@ -13,9 +14,19 @@ function plot_all(positions) {
     // args format: fromDegrees(longitude, latitude, height)
     for (var i = 0; i < positions.length; i++) {
         points.add({
-            position: Cesium.Cartesian3.fromDegrees(positions[i][1], positions[i][0], positions[i][2]),
+            position: Cesium.Cartesian3.fromDegrees(positions[i][1],
+                              positions[i][0], positions[i][2]),
             pixelSize: 1.5,
             color: Cesium.Color.CYAN.withAlpha(0.5)
         });
     }
+
+    // Add ISS path
+    var polylines = viewer.scene.primitives.add(new Cesium.PolylineCollection());
+    polylines.add({
+        show: true,
+        positions: Cesium.Cartesian3.fromDegreesArrayHeights(iss_positions),
+        color: Cesium.Color.RED.withAlpha(0.5),
+        width: 2 
+    });
 }
