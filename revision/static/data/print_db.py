8b3fad28f5d9932c7f9c21eb2e0ff0af5a5db0be
Ryan <ryan.shim@gmial.com>
2018-10-26 12:40:37 -0700

    updated tle data and db testing script

--- revision/static/data/print_db.py
+++ revision/static/data/print_db.py
@@ -7,9 +7,14 @@ conn = sqlite3.connect('tle.db')
 c = conn.cursor()
 count = 0
 
+'''
 for row in c.execute('SELECT * FROM tles'):
     print(row)
     count += 1
+'''
+
+for row in c.execute("SELECT * FROM tles where itl_desig = '98067A'"):
+    print(row)
 
 print(count)
 
@@ -19,9 +24,11 @@ itl_desig = row[0]
 line1 = row[1]
 line2 = row[2]
 
+'''
 sat = ephem.readtle(itl_desig, line1, line2)
 sat.compute()
 lat = np.degrees(sat.sublat)
 lon = np.degrees(sat.sublong)
 height = sat.elevation
 print(lat, lon, height)
+'''
