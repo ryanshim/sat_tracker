2eaef3dfc690081509803e2c3aaad9866b36260e
Ryan <ryan.shim@gmial.com>
2018-10-29 16:27:16 -0700

    WIP: add sat position to globe

--- revision/static/js/create_map.js
+++ revision/static/js/create_map.js
@@ -40,7 +40,8 @@ function update() {
 
 
 // REQUEST DATA
-d3.json('https://gist.githubusercontent.com/d3indepth/f28e1c3a99ea6d84986f35ac8646fac7/raw/c58cede8dab4673c91a3db702d50f7447b373d98/ne_110m_land.json', function(err, json) {
+d3.json('/static/data/ne_110m_land.json', function(err, json) {
   geojson = json;
   window.setInterval(update, 100);
 })
+
