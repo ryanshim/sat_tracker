eb4c053275e516decaed6c9124503abb7547c634
ryan <ryan.shim@gmail.com>
2018-11-06 01:27:38 -0800

    WIP: mercator map of sat position

--- revision/static/js/create_map.js
+++ revision/static/js/create_map.js
@@ -1,17 +1,30 @@
 // Create a map in D3
 // NOTE: using tutorial code for now
-var width = 600;
-var height = 400;
+var w = "600";
+var h = "400";
 
-var svg = d3.select("#map-container").append("svg");
+var svg = d3.select("#map-container").append("svg")
+    .attr("width",w)
+    .attr("height",h)
 
-var projection = d3.geoMercator();
+var projection = d3.geoMercator()
+    .translate([w / 2, h / 2])
+    .scale(w / (2 * Math.PI));
 
-var path = d3.geoPath()
-  .projection(projection);
+var path = d3.geoPath().projection(projection);
+
+var graticule = d3.geoGraticule().step([10, 10]);
 
 var url = "http://enjalot.github.io/wwsd/data/world/world-110m.geojson";
 d3.json(url, function(err, geojson) {
-  svg.append("path")
-    .attr("d", path(geojson))
-})
+
+    var lines = svg.selectAll('path.graticule').data([graticule()]);
+
+    lines.enter().append('path').classed('graticule', true);
+    lines.attr('d', path);
+    lines.exit().remove();
+
+
+    svg.append("path").attr("d", path(geojson));
+});
+
